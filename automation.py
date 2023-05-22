4617e002542b08a5fd057babb0120bb63483be87
Pylos7 <nestor1ingles@gmail.com>
2023-05-22 13:36:11 -0400

    Selenium Basics

--- automation.py
+++ automation.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 service = Service(executable_path='./chromedriver')
 
@@ -9,4 +10,10 @@ options.add_experimental_option("detach", True)
 
 chrome_browser = webdriver.Chrome(options=options, service=service)
 
+chrome_browser.maximize_window()
+
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
+button_text = chrome_browser.find_element(By.CLASS_NAME, "btn-primary")
+
+print(button_text.get_attribute('innerHTML'))
